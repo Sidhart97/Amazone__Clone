3eb01735711c5aaac4035a7f5e630a2d496a6bb5
Sidhart97 <sidharth.sivasree@gmail.com>
2022-09-28 01:37:28 +0530

    checkout__yet_to__complete

---
+++ src/Checkout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/IN_Events/Pc_Affordability-Stripe-5.jpg\\\\\"
+          alt=""
+        />
+        <div>
+          <h2 className="checkout__title">Your shopping Basket </h2>
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log("this basket contains >>>", basket);
+
+  const addToBasket = () => {
+    // dispatch the item to the basket
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +37,9 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button className="basket_button">Add to basket</button>
+      <button className="basket_button" onClick={addToBasket}>
+        Add to basket
+      </button>
     </div>
   );
 }
